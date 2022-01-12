0713de33d53dec05f188cd3a1ade173a2581d392
buramomprakash <96856567+buramomprakash@users.noreply.github.com>
2022-01-12 15:48:11 +0530

    java-training Assesment1

---
+++ Janitor.java
@@ -0,0 +1,74 @@
+package Assesment1;
+
+
+
+class Employee {
+
+    private int baseHours = 40;
+    private double baseSalary = 40000.0;
+    private int baseVacationDays = 10;
+    private String baseVacationForm = "yellow";
+    
+    public int getHours() {
+        return baseHours;                
+    }
+
+    public double getSalary() {
+        return baseSalary;              
+    }
+
+    public int getVacationDays() {
+        return baseVacationDays;        
+    }
+
+    public String getVacationForm() {
+        return baseVacationForm;         
+    }
+    
+    
+    public final void setBaseHours(int hours) {
+        baseHours = hours;
+    }
+    public final void setBaseSalary(double salary) {
+        baseSalary = salary;
+    }
+    public final void setBaseVacationDays(int days) {
+        baseVacationDays = days;
+    }
+    public final void setBaseVacationForm(String form) {
+        baseVacationForm = form;
+    }
+}
+public class Janitor extends Employee {
+    
+    public int getHours() 
+    {
+        return 2 * super.getHours();
+    }
+    
+    public double getSalary() 
+    {
+        return super.getSalary() - 10000;
+    }
+    
+    public int getVacationDays() 
+    {
+        return super.getVacationDays() / 2;
+    }
+    
+    //public void clean() {
+     //   System.out.println("Workin' for the man.");
+   //}
+ 
+
+    public static void main(String[] args)
+    {
+        Janitor janitor = new Janitor();
+        janitor.getHours();
+        janitor.getSalary();
+        janitor.getVacationDays();
+        System.out.println(janitor.getHours());
+        System.out.println(janitor.getSalary());
+        System.out.println(janitor.getVacationDays());
+    }
+}
